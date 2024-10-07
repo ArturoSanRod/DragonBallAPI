5b27582e1b22bc927ed324c6b2e8226574d9b82f
Arturo <arsanmx@gmail.com>
2024-10-07 04:02:13 -0600

    Initial Commit

---
+++ ExamenMoviles/ExamenMovilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenMovilesApp.swift
+//  ExamenMoviles
+//
+//  Created by Arturo Sanchez on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ExamenMovilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExamenMovilesTests/ExamenMovilesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenMovilesTests.swift
+//  ExamenMovilesTests
+//
+//  Created by Arturo Sanchez on 07/10/24.
+//
+
+import Testing
+@testable import ExamenMoviles
+
+struct ExamenMovilesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
